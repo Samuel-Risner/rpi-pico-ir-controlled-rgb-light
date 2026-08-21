a7504b3cbb9a69ff691b827353866e9b0e34f513
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-08-21 16:40:13 +0200

    added comments and functionality and cleaned code up

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from machine import Pin, PWM
 from ir_rx.nec import NEC_16
 
+# GPIO Pins on the Raspberry Pi Pico
 PIN_RED = 18
 PIN_GREEN = 20
 PIN_BLUE = 22
@@ -20,6 +21,7 @@ PWM_RED.freq(PWM_FREQUENCY)
 PWM_GREEN.freq(PWM_FREQUENCY)
 PWM_BLUE.freq(PWM_FREQUENCY)
 
+# initialize to white (step 1/2)
 PWM_RED.duty_u16(MAX_BRIGHTNESS)
 PWM_GREEN.duty_u16(MAX_BRIGHTNESS)
 PWM_BLUE.duty_u16(MAX_BRIGHTNESS)
@@ -27,14 +29,14 @@ PWM_BLUE.duty_u16(MAX_BRIGHTNESS)
 ACTIONS = {
     "00-ef00": ["brightness+", 0.01],
     "01-ef00": ["brightness-", 0.01],
-    "02-ef00": "off",
-    "03-ef00": "on",
-    
+    "02-ef00": ["off"],
+    "03-ef00": ["on"],
+
     "04-ef00": [MAX_BRIGHTNESS, MIN_BRIGHTNESS, MIN_BRIGHTNESS], # red
     "05-ef00": [MIN_BRIGHTNESS, MAX_BRIGHTNESS, MIN_BRIGHTNESS], # green
     "06-ef00": [MIN_BRIGHTNESS, MIN_BRIGHTNESS, MAX_BRIGHTNESS], # blue
     "07-ef00": [MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTNESS], # white
-    
+
     "08-ef00": [0,52428,65535],
     "09-ef00": [62194,28270,54741],
     "0a-ef00": [61423,39321,13878],
@@ -53,6 +55,7 @@ brightness_rel = 1
 MIN_BRIGHTNESS_REL = 0.01
 MAX_BRIGHTNESS_REL = 1
 
+# initialize to white (step 2/2)
 red = MAX_BRIGHTNESS
 green = MAX_BRIGHTNESS
 blue = MAX_BRIGHTNESS
@@ -66,10 +69,10 @@ def callback(data, addr, ctrl):
     global red
     global green
     global blue
-        
+
     key = "{:02x}-{:04x}".format(data, addr)
     action = ACTIONS.get(key)
-        
+
     if action is None:
         print("No action for {}".format(key))
         return
@@ -77,18 +80,19 @@ def callback(data, addr, ctrl):
     print("Action: {}".format(action))
 
     # turn off
-    if action == "off":
+    if action[0] == "off":
         PWM_RED.duty_u16(MIN_BRIGHTNESS)
         PWM_GREEN.duty_u16(MIN_BRIGHTNESS)
         PWM_BLUE.duty_u16(MIN_BRIGHTNESS)
         return
-        
-    # brightness
-    if action[0] == "brightness+":
+
+    # brightness +
+    elif action[0] == "brightness+":
         brightness_rel += action[1]
         if brightness_rel > MAX_BRIGHTNESS_REL:
             brightness_rel = MAX_BRIGHTNESS_REL
-                
+
+    # brightness -
     elif action[0] == "brightness-":
         brightness_rel -= action[1]
         if brightness_rel < MIN_BRIGHTNESS_REL:
@@ -96,10 +100,14 @@ def callback(data, addr, ctrl):
 
     # set RGB values
     else:
-        if action != "on":
+        if action[0] != "on":
             red = action[0]
             green = action[1]
             blue = action[2]
+
+        # reset brightness if turned on again
+        else:
+            brightness_rel = MAX_BRIGHTNESS_REL
         
     PWM_RED.duty_u16(int(red * brightness_rel))
     PWM_GREEN.duty_u16(int(green * brightness_rel))
